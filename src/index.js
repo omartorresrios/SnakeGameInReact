8b1e52fcbd33ce6676cac83acc3aac3e7b9c9ca5
Omar Torres <torresomar44@gmail.com>
2019-05-23 09:59:26 -0500

    Adding skeleton and normalize files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './js/components/App.js';
+import './styles/vendors/normalize.css';
+import './styles/vendors/skeleton.css';
 
 ReactDOM.render(
   <App/>,
