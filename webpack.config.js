e74122540ac29e901b48d805164e01c56964041d
Omar Torres <torresomar44@gmail.com>
2019-05-27 09:48:30 -0500

    production setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
 module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
   module: {
     rules: [
       {
